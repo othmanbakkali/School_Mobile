d6141a3d39500fb249203b5b22ef414392d8d300
othmanbakkali <39952316+othmanbakkali@users.noreply.github.com>
2026-05-04 14:28:28 +0100

    Fix .env loading path in server

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const path = require('path');
-require('dotenv').config();
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 
 const app = express();
 app.use(cors());
